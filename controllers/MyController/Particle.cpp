14a2cbc1c31dbd6c8a4755a1602fec815010ebab
mohamad jalali <sm.jalali.f@gmail.com>
2017-06-29 08:59:14 +0330

    change doaction function in particle

--- controllers/MyController/Particle.cpp
+++ controllers/MyController/Particle.cpp
@@ -5,34 +5,29 @@ Particle::~Particle(void) {
 
 }
 
-void Particle::doAction(Action action, Map *world) {
-    if (action.isRotate) {
+void Particle::doAction(Action *action, Map *world) {
 
-    } else {
+    double step = 0.1;
+    double nextX, nextY;
+    nextX = position.real();
+    nextY = position.imag();
 
+    int iteration = (int) (action->distance / step);
+    for (int j = 0; j < iteration; ++j) {
+        cout << "distanceToNearestObstacle: nextX=" << nextX << "\tnextY=" << nextY << endl;
+        nextX = nextX + cos(angle) * step;
+        nextY = nextY - sin(angle) * step;
 
-        double step = 0.1;
-        double nextX, nextY;
-        nextX = position.real();
-        nextY = position.imag();
-
-        int iteration = (int) (action.amount / step);
-        for (int j = 0; j < iteration; ++j) {
-            cout << "distanceToNearestObstacle: nextX=" << nextX << "\tnextY=" << nextY << endl;
-            nextX = nextX + cos(angle) * step;
-            nextY = nextY - sin(angle) * step;
-
-            if (nextX < 0 || nextX > world->width * (1 / world->scale) || nextY < 0 ||
-                nextY > world->height * (1 / world->scale)) {
-                break;
-            }
-            if (world->canRobotAt(nextX, nextY)) {
-                break;
-            }
-            position = Point(nextX, nextY);
+        if (nextX < 0 || nextX > world->width * (1 / world->scale) || nextY < 0 ||
+            nextY > world->height * (1 / world->scale)) {
+            break;
         }
-
+        if (world->canRobotAt(nextX, nextY)) {
+            break;
+        }
+        position = Point(nextX, nextY);
     }
+    angle = angle + action->rotateRadian;
 }
 
 Particle::Particle(void) {
--- controllers/MyController/ParticleFilter.h
+++ controllers/MyController/ParticleFilter.h
@@ -6,6 +6,7 @@
 #define MYCONTROLLER_PARTICLEFILTER_H
 
 #include <vector>
+#include <random>
 #include "Particle.h"
 #include "Map.h"
 #include "SensorModel.h"
@@ -18,11 +19,17 @@ public:
 
     ParticleFilter(const vector<Particle> &particleSet, Map *map, SensorModel *sensorModel1);
 
-    void updateWeights(Observation &observation) {
-        for (auto &particle : particleSet) {
-            particle.weight = sensorModel->getObservationProbability(&particle, &observation, map);
-        }
-    }
+    void updateWeights(Observation &observation);
+
+    void updateParticleSetWithAction(Action *action);
+
+    void reSampling();
+
+
+    // if d1 - d2 < 10e-9 : return 0;
+    // else if d1 - d2 < 0  : return -1;
+    // else if d1 - d2 > 0  : return +1;
+    int compare(double d1, double d2);
 };
 
 
