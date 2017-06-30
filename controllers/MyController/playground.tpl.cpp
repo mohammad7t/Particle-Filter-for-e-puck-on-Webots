6ffcc7170781b3065c86cd927b641a4f43cb6c6b
mhsekhavat <sekhavat17@gmail.com>
2017-06-30 15:46:49 +0430

    playground tpl

--- controllers/MyController/playground.tpl.cpp
+++ controllers/MyController/playground.tpl.cpp
@@ -1,29 +1,85 @@
 #include "Visualizer.h"
+#include "common.h"
 
-Map map("env-s.txt", "map-s.txt");
-SensorModel sensorModel("sensorMean-i.txt", "sensorVar.txt");
+vector<vector<double>> pathSensors;
+
+Map map("env.txt", "map.txt");
+SensorModel sensorModel("sensorMean-i.txt", "sensorVar-i.txt");
 ParticleFilter particleFilter(10, &map, &sensorModel);
+
+bool once = false;
 
 class Controller : public IController {
 public:
+    int pathI;
+
+    Controller() {
+        pathI = 4;
+    }
+
+    double *nextSensorValues() {
+        double *result = &(pathSensors[pathI][0]);
+        pathI += 10;
+        if (pathI >= pathSensors.size()) {
+            pathI = pathSensors.size() - 100;
+        }
+        return result;
+    }
+
     int nextStep() {
-        cout << "here~~~~~~~~~~~" << endl;
-        //double sensorValues[] = {15.0361, 48.8763, 57.569, 221.986, 235.146, 482.005, 107.171, 16.7376};
-        double sensorValues[] = {15.0361, 10.8763, 10.569, 10.986, 10.146, 10.005, 10.171, 16.7376};
-        Observation observation = particleFilter.sensorModel->sensorValuesToObservation(sensorValues);
+
+        Action action;
+        action.distance = 5;
+        action.rotateRadian = 0;
+        particleFilter.updateParticleSetWithAction(&action);
+        Observation observation = sensorModel.sensorValuesToObservation(nextSensorValues());
+        LOG(observation);
+        Particle &particle = particleFilter.particleSet[0];
+        Map *world = particleFilter.map;
+        Observation expectedObservation = sensorModel.getExpectedObservation(&particle, world);
+        LOG(expectedObservation);
+        particleFilter.updateWeights(observation);
+        LOG(particleFilter.particleSet);
+        /*
         cout << observation << endl;
         particleFilter.updateWeights(observation);
         cout << "updated weights" << endl;
         particleFilter.reSampling();
         cout << "resampled" << endl;
-        _sleep(2000);
-        return 0;
+         */
+        _sleep(1000);
+        return 1;
+
     }
 } controller;
 
 int main(int argc, char **argv) {
+    /**
+    double sensorValues[] = {12.5719,53.7459,49.57,155.656,113.989,495.94,114.787,53.1823};
+    Observation observation = particleFilter.sensorModel->sensorValuesToObservation(sensorValues);
+    //cout << particleFilter.sensorModel->meanSensorVector << endl;
+    cout << observation << endl;
+    return 0;
+    /**/
+
+    ifstream fin("straight-path-sensors.txt");
+    for (int i = 0; i < 700; i++) {
+        vector<double> sensors;
+        for (int j = 0; j < SENSORS; j++) {
+            double value;
+            fin >> value;
+            sensors.push_back(value);
+        }
+        pathSensors.push_back(sensors);
+    }
+
+    cout << particleFilter.map->realWidth << endl;
+    particleFilter.particleSet[0].position = Point(7.2, 7.2);
+    particleFilter.particleSet[0].angle = M_PI_2;
+
     Visualizer visualizer(argc, argv);
     visualizer.visualize(&particleFilter);
     visualizer.runController(&controller);
+    //visualizer.runWithoutController();
     return 0;
 }
